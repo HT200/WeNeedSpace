fbf50fca25fce6e12d99c99452f31a9f9b2bd6ce
nshaffer26 <nshaffer26@outlook.com>
2022-01-31 01:01:18 -0500

    Create a basic collision and score system
    The collision system keeps track of bullets colliding with enemies.
    If a bullet hits an enemy within a radius around its weak point, it will
    be a critical hit.
    
    The player gains score for the following:
    - Hitting an enemy
    - Destroying an enemy with only critical hits (perfect destroy)
    - Clearing a wave (not currently implemented)
    
    The player receives multipliers to their score based on the following:
    - A kill combo based on enemies destroyed without receiving damage
    - Whether or not the player landed a critical hit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    int score;
+
+    // Score Types:
+    // An enemy was hit
+    public int scoreEnemyHit = 5;
+    // An enemy was destroyed with only critical hits
+    public int scorePerfectDestroy = 50;
+    // A wave was cleared
+    public int scoreWaveClear = 100;
+    // TODO: Add more...
+
+    // Multipliers:
+    // How many enemies killed without getting hit
+    int comboMult = 1;
+    // Critically damaged an enemy
+    int critMult = 2;
+    // TODO: Add more...
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Update the player's score
+    /// </summary>
+    public void UpdateScore(int num, bool crit)
+    {
+        // Apply multipliers
+        num *= comboMult;
+        if (crit)
+        {
+            num *= critMult;
+        }
+
+        // Add to the player's score
+        score += num;
+        print("Score: " + score);
+    }
+
+    /// <summary>
+    /// Get the player's current combo multiplier
+    /// </summary>
+    /// <returns>The player's current combo multiplier</returns>
+    public int GetCombo()
+    {
+        return comboMult;
+    }
+    /// <summary>
+    /// Set the player's current combo multiplier
+    /// </summary>
+    public void SetCombo(int newCombo)
+    {
+        comboMult = newCombo;
+    }
+}
