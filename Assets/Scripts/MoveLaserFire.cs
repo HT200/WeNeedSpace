17ccf82eee1982ee82a5309307e7cff66620e044
nds5465 <nds5465@g.rit.edu>
2022-02-07 13:01:12 -0500

    Cleanup system for laser fire (destroy after 5 seconds, this also gives it an effective range of 100m)

--- Assets/Scripts/MoveLaserFire.cs
+++ Assets/Scripts/MoveLaserFire.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class MoveLaserFire : MonoBehaviour
 {
     // Start is called before the first frame update
+    float total;
     void Start()
     {
-        
+        total = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + transform.forward * 10f * Time.deltaTime;
+        transform.position = transform.position + transform.forward * 20f * Time.deltaTime;
+        total += Time.deltaTime;
+        if(total >= 5.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
