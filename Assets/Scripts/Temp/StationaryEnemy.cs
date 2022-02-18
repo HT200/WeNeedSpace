55bd587ba318955ef50abea5a17b0f55f2b796df
nshaffer26 <nshaffer26@outlook.com>
2022-02-17 19:56:10 -0500

    Add temporary script for stationary enemies

---
+++ Assets/Scripts/Temp/StationaryEnemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryEnemy : EnemyController
+{
+    protected override void Start()
+    {
+        
+    }
+
+    protected override void Update()
+    {
+        
+    }
+}
