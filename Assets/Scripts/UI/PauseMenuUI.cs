39f3599febdaeeed200bea622f67272565b1db56
Jackson Blanchard <JacksonBlanchard@github.com>
2022-04-05 23:57:58 -0400

    Removed Temp Scripts folder, added UI and Enemy scripts to respective UI and Enemy Scripts folders

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenuUI : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
