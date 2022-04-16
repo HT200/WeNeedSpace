0ade372df1cd25a2a08c5abc8a4f8f437888d3fd
Gandls42anth <70642446+Gandls42anth@users.noreply.github.com>
2022-04-16 13:56:12 -0400

    Enemy out of bounds logic fixed (continuous update instead of singular)

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -35,6 +35,8 @@ public abstract class EnemyController : MonoBehaviour
     private Vector3 m_target;
     private RaycastHit hit;
 
+    public bool outOfBounds;
+
     public int Health => health;
     protected Vector3 Position => position;
     protected Vector3 Direction => direction;
@@ -53,6 +55,7 @@ public abstract class EnemyController : MonoBehaviour
         m_target = new Vector3(position.x + Random.Range(-30, 30), position.y + Random.Range(-10, 10), fourthToPlayer);
 
         // Debug.Log(m_target);
+        outOfBounds = false;
     }
 
     // Update is called once per frame
@@ -60,7 +63,7 @@ public abstract class EnemyController : MonoBehaviour
     {
         if (m_goSpawn) MoveToSpawnTarget();
         else CalculateSteeringForces();
-    
+        if (outOfBounds) velocity -= position.normalized * 6.0f * Time.deltaTime;  
         UpdatePosition();
         transform.position = position;
     }
